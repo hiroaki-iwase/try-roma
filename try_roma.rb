3dc1da135784cc89766c035184e703ce6896ddd0
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2015-07-23 03:42:37 +0000

    add logical clock count

--- try_roma.rb
+++ try_roma.rb
@@ -5,9 +5,11 @@ require_relative 'balse'
 #require_relative 'post_cmd'
 include TryRomaAPI
 
+configure do
+end
+
 # debug 
 get '/' do
-  #logger.info "version : [#{TryRomaAPI::VERSION}]"
 
   erb :stats
 end
@@ -28,8 +30,12 @@ end
 # get/gets <key>
 get %r{/(get[s]*)/(.*)}  do |i, k|
   if request.cookies[k]
-    logical_clock = 5 if i == 'gets' # toDO
-    @res = "VALUE #{k} 0 #{request.cookies[k].size} #{logical_clock}<br>#{request.cookies[k]}<br>END<br>"
+    h = revert_hash_from_string(request.cookies['hoge'])
+    value = h['value']
+    clk   = h['clk'] if i == 'gets'
+    logger.info "clk = #{clk}"
+
+    @res = "VALUE #{k} 0 #{value.size} #{clk}<br>#{value}<br>END<br>"
   else
     @res = "END<br>"
   end
@@ -59,7 +65,7 @@ end
 #   <digit>/NOT_FOUND => incr, decr
 #
 # Argument Style
-#   <key> <flags> <exptime> <bytes>\r\n<value> : set, add, replace, append, prepend, 
+#   <key> <flags> <exptime> <bytes>\r\n<value> :set, add, replace, append, prepend, 
 #   <key> <flags> <exptime> <bytes> <casid>\r\n<value> : cas
 #   <key> <exptime> : set_expt
 #   <size> : set_size_of_zredundant
@@ -76,19 +82,12 @@ post '/' do
   #params[:size]
   #params[:digit]
 
-  exptime = check_exp_time(exp)
-
   if can_i_set?(cmd, k)
-    case cmd
-    when /^(set|add|replace)$/
-      response.set_cookie(k, :value => v.slice(0, val_size), :expires => exptime)
-    when 'append'
-      pre_v = request.cookies[k]
-      response.set_cookie(k, :value => pre_v.concat(v.slice(0, val_size)), :expires => exptime)
-    when 'prepend'
-      pre_v = request.cookies[k]
-      response.set_cookie(k, :value => pre_v.prepend(v.slice(0, val_size)), :expires => exptime)
-    end
+    exptime = check_exp_time(exp)
+
+    value_hash = value_setting(cmd, k, v, val_size)
+    response.set_cookie(k, :value => value_hash, :expires => exptime)
+
     @res = "STORED"
   else
     @res = "NOT_STORED"
@@ -98,6 +97,32 @@ post '/' do
 end
 
 private
+
+def value_setting(cmd, k, v, val_size)
+  if request.cookies[k]
+    h = revert_hash_from_string(request.cookies['hoge'])
+    pre_v = h['value']
+    pre_clk = h['clk']
+  end
+
+  case cmd
+  when /^(set|add|replace)$/
+    value = v.slice(0, val_size)
+  when 'append'
+    value = pre_v.concat(v.slice(0, val_size))
+  when 'prepend'
+    value = pre_v.prepend(v.slice(0, val_size))
+  end
+
+  if pre_clk
+    h = {'value' => value, 'clk' => pre_clk += 1 }
+  else
+    h =  {'value' => value, 'clk' => 0 }
+  end
+
+  return h 
+end
+
 
 def can_i_set?(command, key)
   if command =~ /^(add)$/
@@ -109,10 +134,24 @@ def can_i_set?(command, key)
 end
 
 def check_exp_time(time)
-  if time > 60 * 60
-    return Time.now + (60 * 60)
+  if time <= 0 || time > 10 * 60
+    return Time.now + (10 * 60)
   else
     return Time.now + time
   end
+end
+
+def revert_hash_from_string(str)
+  if str[0] != '{'
+    raise "Unexpected style : #{str}"
+  end
+
+  str = str.chomp.gsub(/"|^{|}$/, '')
+  str = str.split(/,[\s]*|=>/)
+  str.each_with_index{|column, idx|
+    str[idx] = column.to_i if column =~ /^\d+$/
+  }
+
+  Hash[*str]
 end
 
